934b43a74c76e638e61a81733a46b2f0e1ff8452
Osinachi Chukwujama <Osinachi.Chukwujama@gmail.com>
2024-11-24 00:00:32 +0100

    chore: create repo structure

---
+++ README.md
@@ -0,0 +1,3 @@
+# OpenCV Demos
+
+This repo is so necessary because I don't know jack in OpenCV and I'm really trying to learn the thing pls.
---
+++ capture-or-read-video/capture.py
@@ -0,0 +1,54 @@
+import numpy as np
+import cv2 as cv
+import argparse 
+
+def main(device, capture=False, output_file="output.avi"):
+    cap = cv.VideoCapture(device)
+
+    if not cap.isOpened():
+        print("Error: Cannot open device")
+        return
+    
+    if capture:
+        frame_width = int(cap.get(cv.CAP_PROP_FRAME_WIDTH))
+        frame_height = int(cap.get(cv.CAP_PROP_FRAME_HEIGHT))
+        fps = cap.get(cv.CAP_PROP_FPS) or 30
+
+        fourcc = cv.VideoWriter_fourcc(*'XVID')
+        out = cv.VideoWriter(output_file, fourcc, fps, (frame_width, frame_height), isColor=False)
+    
+    while True:
+        ret, frame = cap.read()
+     
+        if not ret:
+            print("Can't receive frame (stream end?). Exiting ...")
+            break
+
+        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+        if capture:
+            out.write(gray)
+
+        cv.imshow('frame', gray)
+        if cv.waitKey(1) == ord('q'):
+            break
+     
+    cap.release()
+    if capture:
+        out.release()
+    cv.destroyAllWindows()
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Video capture arguments")
+    parser.add_argument("device", help="Camera or video file number")
+    parser.add_argument("--capture", action="store_true", help="Enable video capture")
+    parser.add_argument("--output_file", type=str, help="Name of the video file to write to")
+    args = parser.parse_args()
+
+    device = int(args.device) if args.device == "0" else args.device
+
+    main(device, capture=args.capture, output_file=args.output_file)
+
+
+
+# example python capture.py stretch.avi
